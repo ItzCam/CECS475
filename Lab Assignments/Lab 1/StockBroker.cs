712fb88244a0ded36a8e5bac97b0ea44b13a0504
Antonio Hughes <38517335+ItzCam@users.noreply.github.com>
2021-09-13 10:53:23 -0700

    Update StockBroker.cs
    Updated methods. Now runs properly, just doesn't clear the output.txt file when the program is run every time.

--- Lab Assignments/Lab 1/StockBroker.cs
+++ Lab Assignments/Lab 1/StockBroker.cs
@@ -6,34 +6,22 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.IO;
+using System.Threading;
 
-
-
-namespace stuff
+namespace Lab1
 {
     public class StockBroker
     {
         public string BrokerName { get; set; }
-
         public List<Stock> stocks = new List<Stock>();
-
         public static ReaderWriterLockSlim myLock = new ReaderWriterLockSlim();
-
-        public static SemaphoreSlim sem = new SemaphoreSlim(1);
-
-        // Make sure to use your actual path 
-        readonly string docPath = @"/Users/camwilson/Desktop/CECS475/Lab1_output.txt";
-
-        public string titles = "Broker".PadRight(10) + "Stock".PadRight(15) +
-       "Value".PadRight(10) + "Changes".PadRight(10) + "Date and Time";
+        readonly string docPath = @"/Users/camwilson/Projects/Lab1/Lab1_output.txt";
+        public string titles = "Broker".PadRight(10) + "Stock".PadRight(15) + "Value".PadRight(10) + "Changes".PadRight(10);
 
         /// <summary>
-        /// The stock broker object 
+        /// The stockbroker object
         /// </summary>
         /// <param name="brokerName"> The stockbroker's name </param>
         public StockBroker(string brokerName)
@@ -41,38 +29,54 @@ namespace stuff
             BrokerName = brokerName;
         }
 
-        public event BrokerDelegateHandler BrokerEventHandler;
-
-        public virtual void onBrokerEventChanged()
+        /// <summary>
+        /// Adds stock objects to the stock list
+        /// </summary>
+        /// <param name="stock">Stock object</param>
+        public void AddStock(Stock stock)
         {
-            BrokerDelegateHandler handler = BrokerEventHandler;
-            if (handler != null)
+            stocks.Add(stock);
+            stock.StockEvent += EventHandler;
+        }
+
+        /// <summary>
+        /// The eventhandler that raises the event of a change
+        /// </summary>
+        /// <param name="sender">The sender that indicated a change</param>
+        /// <param name="e">Event arguments</param>
+        void EventHandler(Object sender, EventArgs e)
+        {
+            myLock.EnterWriteLock();
+            try
             {
-                handler(this, EventArgs.Empty);
+                Stock newStock = (Stock)sender;
+
+                string displayCurrentValue = newStock.CurrentValue.ToString();
+                string displayNumChanges = newStock.NumChanges.ToString();
+                string statement = BrokerName.PadRight(10) + newStock.StockName.PadRight(15)
+                    + displayCurrentValue.PadRight(10) + displayNumChanges.PadRight(10);
+
+                Console.WriteLine(statement);
+
+                WriteStockToFile(newStock, displayCurrentValue, displayNumChanges);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("EventHandler failed! Message: {0}", ex.Message);
+            }
+            finally
+            {
+                myLock.ExitWriteLock();
             }
         }
 
-        public void Notify()
+        private void WriteStockToFile(Stock newStock, string displayCurrentValue, string displayNumChanges)
         {
-            Console.WriteLine("{0}\t\t {1}\t\t {2}\t\t {3}", BrokerName);
+            using (StreamWriter outputFile = new StreamWriter(docPath, true))
+            {
+                outputFile.WriteLine(newStock.StockName.PadRight(15) +
+                        displayCurrentValue.PadRight(15) + displayNumChanges);
+            }
         }
-        public void AddStock(Stock s)
-        {
-            //add stocks to the list
-            stocks.Add(s);
-
-            //call stock event
-            s.StockThresholdReachedEvent += StockNotification();
-            Console.WriteLine("Broker \t\t Stock \t\t Value \t\t Changes ");
-        }
-
-        private Stock.StockNotification StockNotification()
-        {
-            throw new NotImplementedException();
-        }
-
-        public delegate void BrokerDelegateHandler(Object sender, EventArgs e);
     }
-
-
 }
